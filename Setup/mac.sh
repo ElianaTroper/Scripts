11299979a99304ddecf3483e24d839a72539c6a7
Eliana Troper <eliana.troper@exactera.com>
2023-09-21 13:34:38 -0400

    Minor updates from current setup

--- Setup/mac.sh
+++ Setup/mac.sh
@@ -1,9 +1,15 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:ElianaTroper/Configs.git $HOME/Configs
+bash $HOME/Configs/setup.sh
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# TODO: This installs it, but it launches a new shell instead of continuing
 
-brew install coreutils
+brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
 
 brew install tmux
 
@@ -11,4 +17,4 @@ brew install tmux
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # TODO: Run linter install script
-# TODO: Install window manager
+brew install --cask rectangle
