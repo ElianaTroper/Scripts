47817303abf76ad80a1c994c0bc4da19e3d6e68d
Eliana Troper <eliana.troper@exactera.com>
2023-10-04 12:15:57 -0400

    Cleaning up linters script

--- Setup/install/linters.sh
+++ Setup/install/linters.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-sudo npm install alex --global
-sudo npm install htmlhint --global
-pip3 install proselint
-# TODO: Only do on linux
-# sudo apt install tidy -y
+brew install vale
